80de9c56e7ea70c9e84446addce22f236d1b1ad1
SixMission <sixmission@proton.me>
2025-02-20 23:24:41 +0100

    Update text

--- YTDL.bat
+++ YTDL.bat
@@ -1,5 +1,4 @@
 @echo off
-pip install --upgrade --target venv\Lib\site-packages pytubefix
-pip install --upgrade --target venv\Lib\site-packages ffmpeg-python
+pip install --upgrade --target venv\Lib\site-packages pytubefix browser-cookie3 ffmpeg-python
 venv\Scripts\python.exe YTDL.py
 pause
