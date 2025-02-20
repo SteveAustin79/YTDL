407a80042a71e18dc6757a727aeead3322b3cc45
SixMission <sixmission@proton.me>
2025-02-20 21:24:22 +0100

    Create YTDL.py

---
+++ YTDL.py
@@ -0,0 +1,26 @@
+from pytubefix import YouTube
+
+try:
+    # Ask the user to input the YouTube URL
+    url = input("Enter the YouTube URL: ")
+
+    yt = YouTube(url)
+
+    print("Title:", yt.title)
+    print("Views:", yt.views)
+    print("\n\n")
+    print(yt.streams.filter(file_extension='mp4'))
+    print("\n\n")
+    videoVersion = input ("Enter itag: ")
+
+    dlpath = input("Enter path to download: ")
+
+    # Get the highest resolution stream
+    yd = yt.streams.get_by_itag(videoVersion)
+
+    # Download the video to the current directory
+    yd.download(output_path=dlpath)
+
+    print("Download complete.")
+except Exception as e:
+    print("An error occurred:", str(e))
