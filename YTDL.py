0c08b8fbab626150c50a4ce54f826ccabe89a204
SixMission <sixmission@proton.me>
2025-02-20 21:50:11 +0100

    Update

--- YTDL.py
+++ YTDL.py
@@ -11,18 +11,28 @@ try:
     print("Title:", yt.title)
     print("Views:", yt.views)
     print("\n")
+    #print(yt.streams.filter(file_extension='mp4'))
+    print(yt.streams)
+    print("\n")
 
     #videoVersion = input ("Enter itag: ")
     res = input("Enter wanted resolution (1080p): ")
 
     dlpath = input("Enter path to download: ")
 
+    # Get the highest resolution stream
+    #yd = yt.streams.get_by_itag(videoVersion)
+    #yd = yt.streams.get_highest_resolution()
+
+    # Download the video to the current directory
+    #yd.download(output_path=dlpath)
+
     for idx, i in enumerate(yt.streams):
         if i.resolution == res:
             #print(idx)
             #print(i.resolution)
             break
-    #print(yt.streams[idx])
+    print(yt.streams[idx])
     yt.streams[idx].download(output_path=dlpath)
 
     print("Download complete.")
