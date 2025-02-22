e6ed7323df1111589286c0cf6ff783cc219bc68b
SixMission <sixmission@proton.me>
2025-02-22 02:39:52 +0100

    Update text

--- YTDL.py
+++ YTDL.py
@@ -59,7 +59,7 @@ try:
     yt = YouTube(url, on_progress_callback = on_progress)
 
     print("Title:", yt.title)
-    print("Views:", yt.description)
+    print("Views:", yt.length)
 
     res = input("Enter desired resolution (eg. 1080p): ")
     dlpath = input("Enter path to download: (eg. d:): ")
