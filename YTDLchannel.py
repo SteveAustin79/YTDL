51dc51063231b5ce3bdc06bd8ff68131abc2521e
SixMission <sixmission@proton.me>
2025-02-25 00:52:39 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -11,7 +11,7 @@ print("***************")
 print("YouTube Channel Downloader (Exit App with Ctrl + C)\n")
 
 YTchannel = input("YouTube Channel URL: ")
-count_fetch_video = input("Fetch x latest Videos: ")
+count_fetch_videos = input("Fetch x latest Videos: ")
 
 c = Channel(YTchannel)
 print(f'\nListing videos by: {c.channel_name}')
@@ -21,18 +21,20 @@ count_restricted_videos = 0
 count_ok_videos = 0
 
 for video in c.videos:
-    count_total_videos = count_total_videos+1
+    count_total_videos += 1
+
     if video.age_restricted == False:
-        count_ok_videos = count_ok_videos + 1
+        count_ok_videos += 1
         video_list.append(video.video_id)
         print(str(count_total_videos) + " - " + video.video_id + " - " + video.title)
     else:
-        count_restricted_videos = count_restricted_videos+1
+        count_restricted_videos += 1
         video_list_restricted.append(video.video_id)
         print("\033[31m" + str(count_total_videos) + " - " + video.video_id + " - " + video.title + "\033[0m")
-    if count_total_videos==count_fetch_video:
+
+    if count_total_videos == count_fetch_videos:
         break
-    break
+
 
 print("Total Videos: " + str(count_total_videos) + ", OK Videos: " + str(count_ok_videos)
       + ", Restricted Videos: " + str(count_restricted_videos))
