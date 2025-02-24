0bdee4a6885e1605b0e67135f4f01010847cee6d
SixMission <sixmission@proton.me>
2025-02-24 23:22:02 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -4,5 +4,8 @@ c = Channel("https://www.youtube.com/@NetworkChuck")
 
 print(f'Lsiting videos by: {c.channel_name}')
 
+i = 0
+
 for video in c.videos:
-    print(video.title + "\n")
+    i = i+1
+    print(str(i) + " - " + video.title)
