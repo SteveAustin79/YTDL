550e0440b3b40d779834641ae1f29ee48707f9bc
SixMission <sixmission@proton.me>
2025-02-24 23:42:04 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -1,5 +1,8 @@
 from pytubefix import Channel
 
+# Create an empty list
+video_list = []
+
 YTchannel = input("YouTube Channel URL: ")
 
 c = Channel(YTchannel)
@@ -10,5 +13,6 @@ i = 0
 for video in c.videos:
     i = i+1
     print(str(i) + " - " + str(video.age_restricted) + " - " + video.video_id + " - " + video.title)
+    video_list.append(video.video_id)
 
-print("Total Videos: ", i)
+print("Total Videos: ", video_list.count())
