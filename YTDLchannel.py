df79be614699aaeb2266f0a0233d91a3d8c82cf8
SixMission <sixmission@proton.me>
2025-02-24 23:58:55 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -6,6 +6,7 @@ version = 0.1
 
 # Create an empty list
 video_list = []
+video_list_restricted = []
 
 print("\nYTDLchannel " + str(version))
 print("***************")
@@ -22,9 +23,10 @@ for video in c.videos:
     i = i+1
     if video.age_restricted == False:
         video_list.append(video.video_id)
-        print(str(i) + " - " + str(video.age_restricted) + " - " + video.video_id + " - " + video.title)
+        print(str(i) + " - " + video.video_id + " - " + video.title)
     else:
-        print("\033[31m" + str(i) + " - " + str(video.age_restricted) + " - " + video.video_id + " - " + video.title + "\033[0m")
+        video_list_restricted.append(video.video_id)
+        print("\033[31m" + str(i) + " - " + video.video_id + " - " + video.title + "\033[0m")
 
 
 print("Total Videos: " + i + ", NAR videos: " + str(video_list.count))
