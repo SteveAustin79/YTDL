ce69917d9d008f8ca9b171b8f7fd16d7e4e455d1
SixMission <sixmission@proton.me>
2025-02-27 22:39:54 +0100

    Update YTDL

--- YTDL.py
+++ YTDL.py
@@ -27,7 +27,7 @@ version = 0.4
 
 class bcolors:
     HEADER     = "\033[95m"
-    OKBLUE     = "\033[94m"
+    OKBLUE     = "\033[96m"
     OKGREEN    = "\033[92m"
     WARNING    = "\033[93m"
     FAIL       = "\033[91m"
