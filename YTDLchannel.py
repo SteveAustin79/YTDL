8c29d284533a24ef8c4d40d579f5b92b77ad24df
SixMission <sixmission@proton.me>
2025-02-28 23:08:48 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -434,7 +434,7 @@ while True:
             ignore_max_duration_bool = True
         elif ignore_max_duration=="n":
             ignore_max_duration_bool = False
-            print(print_colored_text("Ignoring Videos > " + str(max_duration) + " Minutes!"), bcolors.FAIL)
+            print(print_colored_text("Ignoring Videos > " + str(max_duration) + " Minutes!", bcolors.FAIL))
         video_ids = []
         for url in c.video_urls:
             video_ids.append(url.watch_url)
