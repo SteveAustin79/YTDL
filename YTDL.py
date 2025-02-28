14b3d87bd1d8ec1b57a5805c72e8a8fbf169d0c3
SixMission <sixmission@proton.me>
2025-02-28 23:30:51 +0100

    Update YTDL

--- YTDL.py
+++ YTDL.py
@@ -275,7 +275,7 @@ while True:
         print("********")
         print("YouTube Video/Audio Downloader (Exit App with Ctrl + C)")
         print("https://github.com/SteveAustin79/YTDL\n\n")
-        print("Year Subfolder-Structure: ", year_subfolders, "\n\n")
+        print("Year Subfolder-Structure: ", year_subfolders, "\n")
         #cleanup directory
         deletTempFiles()
         url = input("YouTube Video URL: ")
