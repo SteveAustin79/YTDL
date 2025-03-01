ce6b1de4b280c33f4bf18c5139b049b4dab08b87
SteveAustin79 <steveaustin79@proton.me>
2025-03-01 01:18:20 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -278,9 +278,10 @@ def downloadVideo(videoid, counterid, video_total_count):
     yt = YouTube(youtube_base_url + videoid, on_progress_callback=on_progress)
 
     #print("\n***" + str(counterid) + "********************************************************************************")
-    #print(format_header(yt.author + " - " + str(counterid) + "/" + str(video_total_count)))
+    print(format_header(yt.author + " - " + str(counterid) + "/" + str(video_total_count)))
     #print("Channel:    ", yt.author)
-    print("Title:      ", print_colored_text(yt.title, bcolors.OKBLUE))
+    #print("Title:      ", print_colored_text(yt.title, bcolors.OKBLUE))
+    print("Title:      ", yt.title)
     print("Views:      ", format_view_count(yt.views))
     print("Date:       ", yt.publish_date.strftime("%Y-%m-%d"))
     print("Length:     ", str(int(yt.length / 60)) + "m")
