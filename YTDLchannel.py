af51ff1cdaafbd21416be669debac7b7c63b3200
SteveAustin79 <steveaustin79@proton.me>
2025-03-01 16:17:21 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -542,7 +542,7 @@ while True:
         elif skip_restricted=="n":
             skip_restricted_bool = False
 
-        exclude_video_ids_cache = ""
+        #exclude_video_ids_cache = ""
         exclude_video_ids = smart_input("Exclude Video ID's? (comma separated list): ", exclude_video_ids_cache)
         exclude_video_ids_cache = exclude_video_ids
         exclude_list = []
