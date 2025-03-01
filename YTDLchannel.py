46eb055f1eff886446d22fadbb3a0d2c2d235718
SteveAustin79 <steveaustin79@proton.me>
2025-03-01 11:47:03 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -276,7 +276,7 @@ def limit_resolution(resolution, limit):
 
 
 def downloadVideoRestricted(videoid, channelName):
-    yt = YouTube(videoid, use_oauth=True, allow_oauth_cache=True, on_progress_callback = on_progress)
+    yt = YouTube(youtube_base_url + videoid, use_oauth=True, allow_oauth_cache=True, on_progress_callback = on_progress)
     dlpath = smart_input("\nDownload Path:  ", output_dir + "/" + channelName)
     quiet_on = smart_input("Quiet output? Y/n: ", "y")
 
