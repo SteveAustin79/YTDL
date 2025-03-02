896e90286446e5ae7bfefe7332599941c1135b55
SteveAustin79 <steveaustin79@proton.me>
2025-03-02 15:00:04 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -707,12 +707,12 @@ while True:
         if count_this_run == 0:
             print("\n\n" + print_colored_text("nothing to do...\n\n", BCOLORS.OKGREEN))
         else:
-            print(print_colored_text(f"\n\nDONE! Videos: {count_total_videos}, Selected Videos: {count_ok_videos}",
+            print(print_colored_text(f"\n\nDONE!!!", BCOLORS.OKGREEN))
+            print(print_colored_text(f"Videos: {count_total_videos}, Selected Videos: {count_ok_videos}",
                                      BCOLORS.OKGREEN))
             print(print_colored_text(f"Downloaded in this session: {count_this_run}, (restricted: {len(video_list_restricted)} / ignored: {len(video_watch_urls)-count_ok_videos})",
                                      BCOLORS.OKGREEN))
-            print(print_colored_text(f"Remaining disk space: {get_free_space(dlpath)}\n",
-                                     BCOLORS.OKGREEN))
+            print(f"Remaining disk space: {get_free_space(dlpath)}\n")
 
         continue_ytdl = smart_input("Continue?  Y/n ", "y")
         if continue_ytdl=="y":
