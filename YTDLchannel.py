3e07a6cf1197e5df77316ad98944399051c06f25
SteveAustin79 <steveaustin79@proton.me>
2025-03-02 12:06:31 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -704,7 +704,7 @@ while True:
             print("\n\n" + print_colored_text("nothing to do...\n\n", bcolors.OKGREEN))
         else:
             print(print_colored_text(f"\n\nDONE! Total Video: {len(video_watch_urls)}, Selected Videos: {count_ok_videos}", bcolors.OKGREEN))
-            print("Downloaded in this session: {count_this_run}, (restricted: {len(video_list_restricted)} / ignored: {len(video_watch_urls)-count_ok_videos})\n")
+            print(print_colored_text("Downloaded in this session: {count_this_run}, (restricted: {len(video_list_restricted)} / ignored: {len(video_watch_urls)-count_ok_videos})\n"))
 
         continue_ytdl = smart_input("Continue?  Y/n ", "y")
         if continue_ytdl=="y":
