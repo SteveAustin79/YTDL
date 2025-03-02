5b23c4492a52285885f5f9dc614e0449a59c0e33
SteveAustin79 <steveaustin79@proton.me>
2025-03-02 19:17:27 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -537,17 +537,17 @@ while True:
             # Load channel config
             channel_config = load_config(dlpath + "/_config/config_channel.json")
             # Access settings
-            if channel_config["c_max_resolution"] != "":
+            if "c_max_resolution" in channel_config and channel_config["c_max_resolution"] != "":
                 default_max_res = channel_config["c_max_resolution"]
-            if channel_config["c_ignore_min_duration"] != "":
+            if "c_ignore_min_duration" in channel_config and channel_config["c_ignore_min_duration"] != "":
                 default_ignore_min_duration = channel_config["c_ignore_min_duration"]
-            if channel_config["c_ignore_max_duration"] != "":
+            if "c_ignore_max_duration" in channel_config and channel_config["c_ignore_max_duration"] != "":
                 default_ignore_max_duration = channel_config["c_ignore_max_duration"]
-            if channel_config["c_only_restricted"] != "":
+            if "c_only_restricted" in channel_config and channel_config["c_only_restricted"] != "":
                 default_only_restricted = channel_config["c_only_restricted"]
-            if channel_config["c_skip_restricted"] != "":
+            if "c_skip_restricted" in channel_config and channel_config["c_skip_restricted"] != "":
                 default_skip_restricted = channel_config["c_skip_restricted"]
-            if channel_config["c_minimum_views"] != "":
+            if "c_minimum_views" in channel_config and channel_config["c_minimum_views"] != "":
                 default_minimum_views = channel_config["c_minimum_views"]
             default_exclude_videos = channel_config["c_exclude_video_ids"]
             default_include_videos = channel_config["c_include_video_ids"]
