9b63a4a100f1806f1929fe3b311e3316cab69450
SteveAustin79 <steveaustin79@proton.me>
2025-03-02 14:33:38 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -612,7 +612,7 @@ while True:
         else:
             min_video_views_bool = False
 
-        exclude_video_ids = input("Exclude Video ID's (comma separated list): ")
+        exclude_video_ids = input("\nExclude Video ID's (comma separated list): ")
         exclude_list = []
         if exclude_video_ids!="":
             exclude_list = string_to_list(exclude_video_ids)
