78ccc6bf4d769d4956acd12eba274288a13da7dc
SteveAustin79 <steveaustin79@proton.me>
2025-03-03 19:07:27 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -600,7 +600,7 @@ while True:
         #skip_x_videos = int(smart_input("Skip x videos: ", "0"))
 
         c = Channel(YTchannel)
-        print("\n", c.channel_url)
+        print("\n" + str(c.channel_url))
 
         dlpath = smart_input("\nDownload Path:  ", output_dir + "/" + clean_string_regex(c.channel_name).rstrip())
 
