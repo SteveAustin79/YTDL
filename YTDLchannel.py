d9f958961abcfb2ec3ff07603e274c5a865a17b5
SteveAustin79 <steveaustin79@proton.me>
2025-03-05 01:49:52 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -547,7 +547,8 @@ while True:
         print("\n" + print_colored_text(print_colored_text(str(c.channel_name), BCOLORS.BOLD), BCOLORS.CYAN))
         print(print_colored_text(print_colored_text("*" * len(str(c.channel_name)), BCOLORS.BOLD), BCOLORS.CYAN))
 
-        list_all_videos = smart_input("\nList all " + str(len(c.video_urls)) + " Videos?  Y/n", "y")
+        list_all_videos = smart_input("\nList all " + str(len(c.video_urls)) + " Videos? (Restricted videos in "
+                                      + print_colored_text("red", BCOLORS.RED) + ")  Y/n", "y")
         selected_video_ids = []
 
         if list_all_videos == "y":
