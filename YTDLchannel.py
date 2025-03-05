a37cbf7184244c0a4e18165b41d6c07a48725195
SteveAustin79 <steveaustin79@proton.me>
2025-03-06 00:40:51 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -565,10 +565,11 @@ while True:
                 video_list = list(c.videos)  # Convert to a list if not already
 
                 for index, v_video in enumerate(video_list, start=1):
+                    video_list_item =  f"{index}. {v_video.title} ({v_video.publish_date.strftime("%Y-%m-%d")})"
                     if v_video.age_restricted:
-                        print(print_colored_text(f"{index}. {v_video.title} ({v_video.publish_date.strftime("%Y-%m-%d")})", BCOLORS.RED))
+                        print(print_colored_text(video_list_item, BCOLORS.RED))
                     else:
-                        print(f"{index}. {v_video.title} ({v_video.publish_date.strftime("%Y-%m-%d")})")
+                        print(video_list_item)
                 # Ask user for selection
                 while True:
                     try:
