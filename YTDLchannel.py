36d4150d94681f1e8e86f20ff56bf9033c976e73
SteveAustin79 <steveaustin79@proton.me>
2025-03-06 21:34:07 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -260,10 +260,10 @@ def user_selection(u_lines):
 
     print("Select channel:")
     for index, line in enumerate(u_lines, start=1):
-        ytchannel = Channel(line)
-        latest_video = list(ytchannel.videos)
-        latest_date = latest_video[0].publish_date.strftime("%Y-%m-%d")
-        print(f"{index}. {line} (Latest Video: {latest_date})")
+        # ytchannel = Channel(line)
+        # latest_video = list(ytchannel.videos)
+        # latest_date = latest_video[0].publish_date.strftime("%Y-%m-%d")
+        print(f"{index}. {line}")
 
     while True:
         try:
