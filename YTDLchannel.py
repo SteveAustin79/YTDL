16283a78ff629cb24a879f6dbfb8ca3b9c9a680c
SteveAustin79 <steveaustin79@proton.me>
2025-03-06 21:54:37 +0100

    Update YTDL

--- YTDLchannel.py
+++ YTDLchannel.py
@@ -263,7 +263,7 @@ def user_selection(u_lines):
         ytchannel = Channel(line)
         latest_video = list(ytchannel.videos)
         latest_date = latest_video[0].publish_date.strftime("%Y-%m-%d")
-        got_it = find_file_by_string(output_dir + "/" + clean_string_regex(c.channel_name).rstrip(), latest_date, "")
+        got_it = find_file_by_string(output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip(), latest_date, "")
         if got_it:
             latest_date = print_colored_text(latest_date, BCOLORS.GREEN)
         print(f"{index}. {line}   (Latest: {latest_date})")
