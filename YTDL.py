01c71603fc3121f0eea168f2b9cbe2b7ff63c594
SteveAustin79 <steveaustin79@proton.me>
2025-03-12 20:55:05 +0100

    Update YTDL

--- YTDL.py
+++ YTDL.py
@@ -878,13 +878,14 @@ while True:
                         video_watch_urls.append(url.watch_url)
                 print(f"\rFetching " + str(count_total_videos) + " videos", end="", flush=True)
             print(f"\rTotal {count_total_videos} Video(s) by: \033[96m{c.channel_name}\033[0m", end="", flush=True)
-            print("\n")
+            print("")
         for url in video_watch_urls:
             only_video_id = pytubefix.extract.video_id(url)
 
             if find_file_by_string(ytchannel_path, only_video_id, limit_resolution_to, audio_or_video_bool) is not None:
                 count_ok_videos += 1
                 count_skipped += 1
+                print("")
                 print(print_colored_text(f"\rSkipping {count_skipped} Videos", BCOLORS.MAGENTA), end="", flush=True)
             else:
                 do_not_download = 0
