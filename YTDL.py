03195d60c326f858223fbcb679774aca9a26ba19
SteveAustin79 <steveaustin79@proton.me>
2025-03-13 21:21:54 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -9,7 +9,7 @@ from pytubefix import YouTube, Channel
 from pytubefix.cli import on_progress
 
 
-version = "1.1 (20250312)"
+version = "1.1.2 (20250313)"
 header_width_global = 94
 
 class BCOLORS:
