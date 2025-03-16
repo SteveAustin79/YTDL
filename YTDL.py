7dea33793b0fe4cb9ddd6ce2f7c209eeb290c0d4
SteveAustin79 <steveaustin79@proton.me>
2025-03-16 01:05:59 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -907,7 +907,7 @@ while True:
             smart_input("\nEnter filter word(s) (comma separated list): ", default_filter_words))
         video_name_filter_list = string_to_list(video_name_filter)
 
-        print_colored_text(smart_input("Write settings to channel config file?  Y/n", "n"), BCOLORS.BLACK)
+        smart_input("\nSave settings as channel config file?  Y/n", "n")
 
         count_total_videos = 0
         count_restricted_videos = 0
