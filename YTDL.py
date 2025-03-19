10307fd33bc7f71d7d4f4da83fb519d0010a6715
SteveAustin79 <steveaustin79@proton.me>
2025-03-19 01:12:05 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -401,8 +401,8 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                             latest_date = print_colored_text(latest_date, BCOLORS.GREEN)
                         else:
                             latest_date = print_colored_text(latest_date, BCOLORS.RED)
-                        latest_date_formated = ("." * (spaces-len(str(u_index))-len(line)) + "Last: "
-                                                + latest_date + " | " + latest_video[i].video_id)
+                        latest_date_formated = (print_colored_text("." * (spaces-len(str(u_index))-len(line)), BCOLORS.BLACK)
+                                                + "Last: " + latest_date + " | " + latest_video[i].video_id)
                         break
 
         print(f"{u_index}. {line}{latest_date_formated}")
