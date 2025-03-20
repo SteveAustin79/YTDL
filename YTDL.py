19fa7a1ba0020c51cc8d74509a48e68f9c64af9c
SteveAustin79 <steveaustin79@proton.me>
2025-03-20 22:50:27 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -416,7 +416,7 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                     latest_date_formated = (" " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 2), BCOLORS.BLACK)
                                             + " " + print_colored_text(str(eee), BCOLORS.RED))
 
-        line = line.replace(youtube_url, "")[1:]
+        line = line.replace(youtube_url, "")[0:]
         print(f"{u_index}. {line}{latest_date_formated}")
         latest_date_formated = ""
 
