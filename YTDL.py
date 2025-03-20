1977dd11c36b0624b6c018586e61100da5226809
SteveAustin79 <steveaustin79@proton.me>
2025-03-20 21:58:49 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -407,8 +407,8 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                             else:
                                 latest_date = print_colored_text(latest_date, BCOLORS.RED)
                             latest_date_formated = (" " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 2), BCOLORS.BLACK)
-                                                    + " Latest: " + latest_date + print_colored_text(" | ", BCOLORS.BLACK) + latest_video[i].video_id)
-                                                    #"\n" + print_colored_text(latest_video_name.rjust(header_width_global), BCOLORS.BLACK))
+                                                    + " Latest: " + latest_date + print_colored_text(" | ", BCOLORS.BLACK) + latest_video[i].video_id
+                                                    + print_colored_text(latest_video_name[:20], BCOLORS.BLACK))
                             break
                 except Exception as eee:
                     latest_date_formated = (" " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 2), BCOLORS.BLACK)
