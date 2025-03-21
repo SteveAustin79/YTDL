5795c1e1f1dbc20b710a6d98d4146e3843efa02f
SteveAustin79 <steveaustin79@proton.me>
2025-03-21 23:46:51 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -393,17 +393,29 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
         if not line == u_lines[(len(u_lines) - 1)]:
             c_year_active = print_colored_text(".", BCOLORS.BLACK)
             c_restricted_active = print_colored_text(".", BCOLORS.BLACK)
+            c_resolution_active = print_colored_text(".", BCOLORS.BLACK)
             if u_show_latest_video_date:
                 spaces = (header_width_global - 61)
                 ytchannel = Channel(line)
                 if os.path.exists(output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip() + channel_config_path):
                     c_year_active = print_colored_text("-", BCOLORS.BLUE)
                     c_restricted_active = print_colored_text("-", BCOLORS.RED)
+                    c_resolution_active = print_colored_text("-", BCOLORS.YELLOW)
+
                     ch_config = load_config(output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip() + channel_config_path)
+
                     if ch_config["c_year_subfolders"]=="y":
                         c_year_active = print_colored_text("Y", BCOLORS.BLUE)
                     if ch_config["c_only_restricted"]=="y":
                         c_restricted_active = print_colored_text("R", BCOLORS.RED)
+                    if ch_config["c_max_resolution"]=="max":
+                        c_restricted_active = print_colored_text("M", BCOLORS.YELLOW)
+                    elif ch_config["c_max_resolution"]=="1080p":
+                        c_restricted_active = print_colored_text("1", BCOLORS.YELLOW)
+                    elif ch_config["c_max_resolution"]=="720p":
+                        c_restricted_active = print_colored_text("7", BCOLORS.YELLOW)
+                    elif ch_config["c_max_resolution"]=="2160p":
+                        c_restricted_active = print_colored_text("2", BCOLORS.YELLOW)
 
                 line = line.replace(youtube_url, "")[1:]
                 try:
@@ -420,8 +432,8 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                             else:
                                 latest_date = print_colored_text(latest_date, BCOLORS.RED)
                             latest_date_formated = (
-                                    " " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 4), BCOLORS.BLACK)
-                                    + c_restricted_active + c_year_active + " " + latest_date + print_colored_text(" | ", BCOLORS.BLACK)
+                                    " " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 5), BCOLORS.BLACK)
+                                    + c_resolution_active + c_restricted_active + c_year_active + " " + latest_date + print_colored_text(" | ", BCOLORS.BLACK)
                                     + print_colored_text(latest_video[i].video_id, BCOLORS.BLACK) + print_colored_text(" | ", BCOLORS.BLACK)
                                     + print_colored_text(latest_video_name[:32], BCOLORS.BLACK))
                             break
