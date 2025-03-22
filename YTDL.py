4240849df57f9175450a24cb249527a0d796a4bb
SteveAustin79 <steveaustin79@proton.me>
2025-03-22 23:13:16 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -261,7 +261,8 @@ def load_config(c_file: str):
 def print_asteriks_line() -> None:
     # formatted = f"{counter_str.ljust(total_length, '*')}"
     # print("*" * header_width_global)
-    print(f"{get_free_space(output_dir).ljust(header_width_global, '*')}")
+    free_space = " " + get_free_space(output_dir) + " "
+    print(f"{free_space.center(header_width_global, '*')}")
 
 def print_colored_text(message_text: str, color: str) -> str:
     return f"{color}{message_text}{BCOLORS.ENDC}"
