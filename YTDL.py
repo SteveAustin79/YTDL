fa8e81da3a141c3362dd148609e74b7197246e75
SteveAustin79 <steveaustin79@proton.me>
2025-03-22 20:49:34 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -452,9 +452,9 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                         c_resolution_active = print_colored_text("2", BCOLORS.YELLOW)
 
                     if ch_config["c_filter_words"]!="":
-                        c_filter_words_active = print_colored_text(ch_config["c_filter_words"], BCOLORS.GREEN)
+                        c_filter_words_active = print_colored_text(ch_config["c_filter_words"][:7], BCOLORS.GREEN)
 
-                combined_settings = c_filter_words_active.ljust(7)[:7] + c_resolution_active + c_restricted_active + c_year_active
+                combined_settings = c_filter_words_active.ljust(7) + c_resolution_active + c_restricted_active + c_year_active
 
                 line = line.replace(youtube_url, "")[1:]
                 got_it = False
