02876e13f268cec749508e771dc5189870b512cd
SteveAustin79 <steveaustin79@proton.me>
2025-03-22 14:53:08 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -455,13 +455,12 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                     for i in range(len(latest_video)):
                         if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
                                 latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE'):
-                            latest_date = latest_video[i].publish_date.strftime("%Y-%m-%d")
                             latest_video_name = latest_video[i].title
                             got_it = find_file_by_string(
                                 output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip(), latest_date, "", False)
-                            latest_date = print_colored_text(latest_date, BCOLORS.RED)
-                            if got_it:
-                                latest_date = print_colored_text(latest_date, BCOLORS.BLACK)
+                            latest_date = latest_video[i].publish_date.strftime("%Y-%m-%d")
+                            if not got_it:
+                                latest_date = print_colored_text(latest_date, BCOLORS.RED)
                             latest_date_formated = (
                                     " " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 5), BCOLORS.BLACK)
                                     + c_resolution_active + c_restricted_active + c_year_active + " " + latest_date + print_colored_text(" | ", BCOLORS.BLACK)
