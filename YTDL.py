b16defc06ea20faed75db1c9b4ece9281c873728
SteveAustin79 <steveaustin79@proton.me>
2025-03-23 17:33:19 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -522,7 +522,8 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                         # if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
                         #         latest_video[i].vid_info.get('playabilityStatus', {}).get(
                         #             'status') != 'LIVE_STREAM_OFFLINE'):
-                        print(print_colored_text(f"\r" + " " * len(str(u_index)) + "Checking " + str(len(latest_video)) + " videos from " + latest_video[i].author + " " +
+                        print(print_colored_text(f"\r" + " " * len(str(u_index)) + "Checking " + str(i) + "/" +
+                                    str(len(latest_video)) + " videos from " + latest_video[i].author + " " +
                                     latest_video[i].video_id, BCOLORS.BLACK), end="", flush=True)
                         if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
                                 latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE' and
