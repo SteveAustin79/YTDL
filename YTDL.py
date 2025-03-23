b963553daece636b35c4b67154e7bd35ecae7bdb
SteveAustin79 <steveaustin79@proton.me>
2025-03-23 18:42:36 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from datetime import datetime
 
 version = "1.3.4 (20250323)"
-header_width_global = 97
+header_width_global = 99
 first_column_width = 17
 first_column_width_wide = 37
 youtube_url = "https://www.youtube.com/"
@@ -533,13 +533,13 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                             latest_date_math = latest_video[i].publish_date.strftime(date_format_math)
                             latest_date = latest_video[i].publish_date.strftime(date_format_display)
                             channel_total_videos = " " + str(len(latest_video)).rjust(5)[:5] + " | "
-                            latest_id_and_name = " | " + latest_video[i].video_id + " | " + latest_video_name[:13]
+                            latest_id_and_name = " | " + latest_video[i].video_id + " | " + latest_video_name[:15]
                             got_it = find_file_by_string(
                                 output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip(), latest_date_math, "", False)
                             if not got_it:
                                 latest_date = print_colored_text(latest_date, BCOLORS.RED)
                                 latest_id_and_name = print_colored_text(" | " + latest_video[i].video_id + " | "
-                                                                        + latest_video_name[:13], BCOLORS.DARK_WHITE)
+                                                                        + latest_video_name[:15], BCOLORS.DARK_WHITE)
                                 channel_total_videos = print_colored_text(channel_total_videos, BCOLORS.DARK_WHITE)
                             latest_date_formated = (
                                     " " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 16), BCOLORS.BLACK)
