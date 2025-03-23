643a080fd490f727fc418c1536a1ff93a5eddfb5
SteveAustin79 <steveaustin79@proton.me>
2025-03-23 12:11:04 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -1286,21 +1286,21 @@ while True:
                         word.lower() in video.title.lower() for word in video_name_filter_list):
                     if not ignore_min_duration_bool:
                         video_duration = int(video.length / 60)
-                        if video_duration < int(min_duration):
+                        if video_duration <= int(min_duration):
                             do_not_download = 1
 
                     if not ignore_max_duration_bool:
                         video_duration = int(video.length / 60)
-                        if video_duration > int(max_duration):
+                        if video_duration >= int(max_duration):
                             do_not_download = 1
 
                     if int(min_year) > 0:
-                        if int(video.publish_date.strftime("%Y")) < int(min_year):
+                        if int(video.publish_date.strftime("%Y")) <= int(min_year):
                             do_not_download = 1
                             break
 
                     if int(max_year) > 0:
-                        if int(video.publish_date.strftime("%Y")) > int(max_year):
+                        if int(video.publish_date.strftime("%Y")) >= int(max_year):
                             do_not_download = 1
 
                     if min_video_views > 0:
