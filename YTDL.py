b3c12567d492c4f619619f39b047e926a3192cad
SteveAustin79 <steveaustin79@proton.me>
2025-03-23 03:09:25 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -492,11 +492,13 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                     latest_video = list(ytchannel.videos)
                     for i in range(len(latest_video)):
                         # check here filters (filter words, year etc)
+                        # if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
+                        #         latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE' and
+                        #         any(word.lower() in latest_video[i].title.lower() for word in string_to_list(ch_config["c_filter_words"]))
+                        #         and int(main_config["min_duration_in_minutes"]) < int(
+                        #             latest_video[i].length / 60) < int(main_config["max_duration_in_minutes"])):
                         if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
-                                latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE' and
-                                any(word.lower() in latest_video[i].title.lower() for word in string_to_list(ch_config["c_filter_words"]))
-                                and int(main_config["min_duration_in_minutes"]) < int(
-                                    latest_video[i].length / 60) < int(main_config["max_duration_in_minutes"])):
+                                latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE'):
                             latest_video_name = latest_video[i].title
                             latest_date_math = latest_video[i].publish_date.strftime(date_format_math)
                             latest_date = latest_video[i].publish_date.strftime(date_format_display)
