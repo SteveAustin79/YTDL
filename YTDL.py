4e8d4805c57eaf21e1ab55432b687f5693f47499
SteveAustin79 <steveaustin79@proton.me>
2025-03-23 18:08:49 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -532,16 +532,16 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                             latest_video_name = latest_video[i].title
                             latest_date_math = latest_video[i].publish_date.strftime(date_format_math)
                             latest_date = latest_video[i].publish_date.strftime(date_format_display)
-                            latest_id_and_name = (latest_video[i].video_id + " | " + latest_video_name[:15])
+                            latest_id_and_name = (latest_video[i].video_id + " | " + latest_video_name[:14])
                             got_it = find_file_by_string(
                                 output_dir + "/" + clean_string_regex(ytchannel.channel_name).rstrip(), latest_date_math, "", False)
                             if not got_it:
                                 latest_date = print_colored_text(latest_date, BCOLORS.RED)
                                 latest_id_and_name = print_colored_text(latest_video[i].video_id + " | "
-                                                                        + latest_video_name[:15], BCOLORS.DARK_WHITE)
+                                                                        + latest_video_name[:14], BCOLORS.DARK_WHITE)
                             latest_date_formated = (
                                     " " + print_colored_text("." * ((spaces - len(str(u_index)) - len(line)) - 16), BCOLORS.BLACK)
-                                    + combined_settings + " (" + str(len(latest_video)).rjust(5)[:5] + ") " + print_colored_text(latest_date, BCOLORS.BLACK)
+                                    + combined_settings + " | " + str(len(latest_video)).rjust(5)[:5] + " | " + print_colored_text(latest_date, BCOLORS.BLACK)
                                     + " | " + print_colored_text(latest_id_and_name, BCOLORS.BLACK))
                             break
                     if got_it:
