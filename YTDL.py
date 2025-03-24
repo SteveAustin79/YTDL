46f75313eb7e3cc2e4a2d6517932d758e5100b93
SteveAustin79 <steveaustin79@proton.me>
2025-03-24 02:17:10 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -527,8 +527,8 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
                         print(print_colored_text(f"\r" + " " * (len(str(u_index)) + 2) + "Find match: ", BCOLORS.DARK_GREEN) +
                                 print_colored_text(print_colored_text(str(i + 1) + "/" + str(len(latest_video)) + " | " +
                                 latest_video[i].video_id, BCOLORS.GREEN), BCOLORS.BOLD) +
-                                print_colored_text(" | " + str(latest_video[i].author)[:20] + " | "
-                                                   + str(latest_video[i].title).ljust(39, " ")[:39], BCOLORS.DARK_GREEN), end="", flush=True)
+                                print_colored_text(" | " + str(str(latest_video[i].author)[:20] + " | "
+                                                   + latest_video[i].title).ljust(59, " ")[:59], BCOLORS.DARK_GREEN), end="", flush=True)
                         if (latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'UNPLAYABLE' and
                                 latest_video[i].vid_info.get('playabilityStatus', {}).get('status') != 'LIVE_STREAM_OFFLINE' and
                                 any(word.lower() in latest_video[i].title.lower() for word in string_to_list(ch_config_filter_words))
