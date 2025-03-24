980eb642526e89ea4483937cd4fe3ec68373582e
SteveAustin79 <steveaustin79@proton.me>
2025-03-24 18:10:31 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -683,7 +683,6 @@ def download_video(channel_name: str, video_id: str, counter_id: int, video_tota
     except Exception as eee:
         publishing_date = eee
 
-    print(yt.fmt_streams)
     # print(yt.vid_info.get('playabilityStatus'))
     # print(yt.streams.filter(file_extension='mp4'))
     # print(str(yt.streams))
