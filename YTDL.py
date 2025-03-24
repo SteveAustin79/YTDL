011c8a94f290861dd930f05eecd5709a1769cc1f
SteveAustin79 <steveaustin79@proton.me>
2025-03-24 19:08:38 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -685,8 +685,7 @@ def download_video(channel_name: str, video_id: str, counter_id: int, video_tota
 
     # print(yt.vid_info.get('playabilityStatus'))
     # print(yt.streams.filter(file_extension='mp4'))
-    print("STR:")
-    print(str(yt.streams))
+    # print(str(yt.streams))
 
     if year_subfolders:
         year = "/" + str(yt.publish_date.strftime("%Y"))
