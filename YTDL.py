064ec27da432f69f25779f985795b1c950a3e184
SteveAustin79 <steveaustin79@proton.me>
2025-03-27 21:43:44 +0100

    updated YTDL

--- YTDL.py
+++ YTDL.py
@@ -547,7 +547,7 @@ def user_selection(u_lines, u_show_latest_video_date: bool):
 
     temp_disable = "y"
     if show_latest_video_date:
-        temp_disable = smart_input("Skip latest Video date for this run?  Y/n", "y")
+        temp_disable = smart_input("Skip latest Video date for this run?  Y/n", "n")
         for _ in range(12):     # 7 config line, 3 header/footer, 3 spaces = 13
             sys.stdout.write(BCOLORS.MOVES1UP)  # Move cursor up
             sys.stdout.write(BCOLORS.CLRLINE)  # Clear the line
